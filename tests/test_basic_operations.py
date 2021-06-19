cd2dd30a8b6b9de508cd99dca25e69d937104513
oscar <vergil.ferrer.jp@gmail.com>
2021-06-19 15:08:40 +0900

    renamed folder: test to tests

---
+++ tests/test_basic_operations.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Unit tests for the basic_operations library
+"""
+
+
+from lib import basic_operations
+
+
+class TestBasicOperations(object):
+
+    def test_add(self):
+        assert 3 == add(1, 2)
+        assert -3 == add(-1, -2)
+        assert 0 == add(1, -1)
+
+    def test_subtract(self):
+        assert 1 == subtract(3, 2)
+        assert 1 == subtract(-1, -2)
+        assert 2 == subtract(1, -1)
