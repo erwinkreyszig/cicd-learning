e06f69ca401d7fd2d8183a751cc882131f751286
oscar <vergil.ferrer.jp@gmail.com>
2021-06-19 14:31:13 +0900

    fixed line separations

--- lib/basic_operations.py
+++ lib/basic_operations.py
@@ -7,24 +7,29 @@ def add(val1, val2):
         raise Exception
     return val1 + val2
 
+
 def subtract(val1, val2):
     """Subtracts the second argument from the first argument"""
     if type(val1) != int or type(val2) != int:
         raise Exception
     return val1 - val2
 
+
 def multiply(val1, val2):
     """Multiplies 2 int numbers and returns the result"""
     return val1 * val2
+
 
 def divide(val1, val2):
     """Divide an int number by another"""
     return val1 / val2
 
+
 def test():
     assert 3 == add(1, 2)
     assert -3 == add(-1, -2)
     assert 0 == add(1, -1)
+
 
 def test_subtract():
     assert 1 == subtract(3, 2)
