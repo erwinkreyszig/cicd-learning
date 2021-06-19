1e435fd9dd3dd5cb7a53c02ab9c262763d3b64a9
yn0099 <67992608+yn0099@users.noreply.github.com>
2021-06-19 23:15:52 +0900

    I changed instance methods into static methods

--- lib/basic_operations.py
+++ lib/basic_operations.py
@@ -3,32 +3,32 @@
 
 class BasicOperations:
 
-
-    def add(self, val1, val2):
+    @staticmethod
+    def add(val1, val2):
         """Adds 2 int numbers and returns the result"""
         if type(val1) != int or type(val2) != int:
             raise Exception
         return val1 + val2
 
-
-    def subtract(self, val1, val2):
+    @staticmethod
+    def subtract(val1, val2):
         """Subtracts the second argument from the first argument"""
         if type(val1) != int or type(val2) != int:
             raise Exception
         return val1 - val2
 
-
-    def multiply(self, val1, val2):
+    @staticmethod
+    def multiply(val1, val2):
         """Multiplies 2 int numbers and returns the result"""
         return val1 * val2
 
-
-    def divide(self, val1, val2):
+    @staticmethod
+    def divide(val1, val2):
         """Divides an int number by another, then returns a tuple of quotient and remainder"""
-	      quotient = int(val1 / val2)
-	      remainder = int(val1 % val2)
-	      t = (quotient, remainder) 
-	      return t
+        quotient = int(val1 / val2)
+        remainder = int(val1 % val2)
+        t = (quotient, remainder) 
+        return t
 
 
 if __name__ == '__main__':
