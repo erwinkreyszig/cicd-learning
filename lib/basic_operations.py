05ac66af26d348bbb166ce871b8d99ffc1aeb0f8
yn0099 <67992608+yn0099@users.noreply.github.com>
2021-06-19 21:18:47 +0900

    I made 4 methods into instance methods and put them in a class

--- lib/basic_operations.py
+++ lib/basic_operations.py
@@ -1,31 +1,36 @@
 # -*- coding: utf-8 -*-
 
 
-def add(val1, val2):
-    """Adds 2 int numbers and returns the result"""
-    if type(val1) != int or type(val2) != int:
-        raise Exception
-    return val1 + val2
+class BasicOperations:
 
 
-def subtract(val1, val2):
-    """Subtracts the second argument from the first argument"""
-    if type(val1) != int or type(val2) != int:
-        raise Exception
-    return val1 - val2
+    def add(self, val1, val2):
+        """Adds 2 int numbers and returns the result"""
+        if type(val1) != int or type(val2) != int:
+            raise Exception
+        return val1 + val2
 
 
-def multiply(val1, val2):
-    """Multiplies 2 int numbers and returns the result"""
-    return val1 * val2
+    def subtract(self, val1, val2):
+        """Subtracts the second argument from the first argument"""
+        if type(val1) != int or type(val2) != int:
+            raise Exception
+        return val1 - val2
 
 
-def divide(val1, val2):
-    """Divide an int number by another"""
-    return val1 / val2
+    def multiply(self, val1, val2):
+        """Multiplies 2 int numbers and returns the result"""
+        return val1 * val2
+
+
+    def divide(self, val1, val2):
+        """Divide an int number by another"""
+        return val1 / val2
 
 
 if __name__ == '__main__':
-    print(multiply(2, 4))
-    print(multiply(-4, -8))
-    print(multiply(4, -9))
+    operation = BasicOperations()
+    print(operation.subtract(2, 4))
+    print(operation.add(-4, -8))
+    print(operation.multiply(4, -9))
+    print(operation.divide(9, 2))
