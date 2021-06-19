d4330684de6d7dded05249504c13228664df245a
oscar <vergil.ferrer.jp@gmail.com>
2021-06-19 15:28:11 +0900

    changed directories for accessing files

--- tests/test_basic_operations.py
+++ tests/test_basic_operations.py
@@ -4,7 +4,7 @@ Unit tests for the basic_operations library
 """
 
 
-from lib.basic_operations import add, subtract
+from src.lib.basic_operations import add, subtract
 
 
 class TestBasicOperations(object):
